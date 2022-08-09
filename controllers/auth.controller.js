1f41a65a3cc6d507742c617bb8672e5066a49e27
Nazho69 <nicolas.ignacio.aguila@gmail.com>
2022-08-09 16:27:41 -0400

    Update a prueba URL

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,8 @@
 export const register = (req, res) => {
   console.log(req.body);
-  res.json({ ok: "Register" });
+  res.json({ ok: "Prueba Register Pasada" });
 };
 
 export const login = (req, res) => {
-  res.json({ ok: "Login" });
+  res.json({ ok: "Prueba Login Pasada" });
 };
