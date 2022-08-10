bf28801a9e11b978709e8bffae6e62c7ec9fbaf9
NicolasAguila <ignacio.aguila.parra@gmail.com>
2022-08-10 01:32:02 -0400

    add generate token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,44 +1,49 @@
 import { User } from '../models/User.js';
+import jwt from 'jsonwebtoken';
 
 export const register = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    // alternativa buscando por email
-    let user = await User.findOne({ email });
-    if (user) throw ({ code: 11000 });
+    const { email, password } = req.body;
+    try {
+        // alternativa buscando por email
+        let user = await User.findOne({ email });
+        if (user) throw ({ code: 11000 });
 
-    user = new User({ email, password });
-    await user.save();
+        user = new User({ email, password });
+        await user.save();
 
-    //JWT token
+        //JWT token
 
-    return res.status(201).json({ ok: true });
-  } catch (error) {
-    console.log(error);
-    // Alternativa por defecto mongoose
-    if (error.code === 11000) {
-      return res.status(400).json({ error: "Ya existe este usuario" });
+        return res.status(201).json({ ok: true });
+    } catch (error) {
+        console.log(error);
+        // Alternativa por defecto mongoose
+        if (error.code === 11000) {
+            return res.status(400).json({ error: "Ya existe este usuario" });
+        }
+        return res.status(500).json({ error: "Error de servidor" });
     }
-    return res.status(500).json({ error: "Error de servidor" });
-  }
 };
 
 export const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    let user = await User.findOne({ email });
-    if (!user)
-      return res.status(403).json({ error: "No existe este usuario" });
+        let user = await User.findOne({ email });
+        if (!user)
+            return res.status(403).json({ error: "No existe este usuario" });
 
-    const respuestaPassword = await user.comparePassword(password);
-    if(!respuestaPassword)
-      return res.status(403).json({ error: "Contraseña incorrecta" });
-    
-    return res.json({ ok: "Login" })
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ error: "Error de servidor" });
-  }
+        const respuestaPassword = await user.comparePassword(password);
+        if (!respuestaPassword)
+            return res.status(403).json({ error: "Contraseña incorrecta" });
+
+        // Generar el token JWT
+        const token = jwt.sign({uid: user._id}, process.env.JWT_SECRET);
+
+
+        return res.json({ token });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Error de servidor" });
+    }
 
 };
