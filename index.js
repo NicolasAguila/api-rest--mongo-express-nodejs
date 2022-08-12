329e88403270d6987722d2d1c982881fe7965c22
NicolasAguila <ignacio.aguila.parra@gmail.com>
2022-08-12 00:23:53 -0400

    add module link

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import './database/connectdb.js';
 import express from 'express';
 import authRouter from './routes/auth.route.js';
 import cookieParser from 'cookie-parser';
+import linkRouter from "./routes/link.route.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/links', linkRouter);
 
 // Solo para el ejemplo de login y token
 app.use(express.static('public'));
