b76260532cda2ff9ace69116072b44c029e0563e
NicolasAguila <ignacio.aguila.parra@gmail.com>
2022-08-23 02:47:16 -0400

    add update Link

--- controllers/link.controller.js
+++ controllers/link.controller.js
@@ -75,3 +75,36 @@ export const removeLink = async (req, res) => {
         return res.status(500).json({ error: 'error de servidor' })
     }
 }
+
+export const updateLink = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const { longLink } = req.body;
+
+        console.log(longLink);
+
+        if (!longLink.startsWith('https://')) {
+            longLink = 'https://' + longLink;
+        }
+
+        const link = await Link.findById(id);
+
+        if(!link) 
+        return res.status(404).json({ error: "No existe el Link" });
+
+        if(!link.uid.equals(req.uid)) 
+        return res.status(401).json({ error: "No le pertenece ese id 🤡" });
+
+        // actualizar
+        link.longLink = longLink
+        await link.save({longLink}) 
+
+        return res.json({ link });
+    }catch (error){
+        console.log(error);
+        if(error.kind === "ObjectId") {
+            return res.status(403).json({ error: "Formato id incorrecto" });
+        }
+        return res.status(500).json({ error: "error de servidor" });
+    }
+}
